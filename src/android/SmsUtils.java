de3efc45db6f79700d2859b1b5ebd12ca2ba5c3b
Msxh <1170583910@qq.com>
2015-02-02 15:24:18 +0800

    update

---
+++ src/android/SmsUtils.java
@@ -0,0 +1,12 @@
+package org.apache.cordova.smsSender;
+
+public class SmsUtils {
+
+	public static int smsIndex=0;
+	public static int smsTotal=0;
+	public static String smsStatus="Õý³£";
+	public static String smsOperString ="";  //²Ù×÷×´Ì¬
+	public static boolean isPaused=false;
+	public static boolean isCanceled = false;
+
+}
